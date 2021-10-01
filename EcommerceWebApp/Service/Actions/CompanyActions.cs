9ffd7433cade435196fd6504743b845665aa23df
Simon Wondimu <simon.wondimu@cronos.be>
2021-10-01 15:19:31 +0200

    company entity added

--- EcommerceWebApp/Service/Actions/CompanyActions.cs
+++ EcommerceWebApp/Service/Actions/CompanyActions.cs
@@ -4,7 +4,15 @@ namespace EcommerceWebApp.Service.Actions
 {
     public class Company : EntityBase
     {
-
+        public string Name { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string County { get; set; }
+        public string City { get; set; }
+        public string Tin { get; set; }
+        public string Trn { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
     }
     public class CompanyActions : CustomPersistentObjectActionsReference<EcommerceWebAppContext, Company>
     {
