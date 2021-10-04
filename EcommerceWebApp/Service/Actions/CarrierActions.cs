c5132db69b627386dd5058a2fa43c90b57fbcd30
Simon Wondimu <simon.wondimu@cronos.be>
2021-10-04 12:58:57 +0200

    added entity Carrier

---
+++ EcommerceWebApp/Service/Actions/CarrierActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace EcommerceWebApp.Service.Actions
+{
+    public class Carrier : EntityBase
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string DeliveryText { get; set; }
+        public string Logo { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+    public class CarrierActions : CustomPersistentObjectActionsReference<EcommerceWebAppContext, Carrier>
+    {
+        public CarrierActions(EcommerceWebAppContext context) : base(context) { }
+    }
+}
